fc5a6ca6089870eed8ddd0715e503dd143e7a3ae
unknown <emprenderusinque@hotmail.com>
2022-12-10 11:28:50 -0500

    Manejar un objeto como estado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Counter } from './bases/Counter'
+import { CounterBy } from './bases/CounterBy';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>React</h1>
       <hr/>
       <Counter initialValue={ 15 } />
+      <CounterBy/>
     </>
   )
 }
